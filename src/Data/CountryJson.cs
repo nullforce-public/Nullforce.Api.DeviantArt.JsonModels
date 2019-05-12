9fb9584ba6b759dd404365ea539e1b45bc28bb7e
nullforce <glenngit@nullforce.com>
2019-05-12 00:44:03 -0500

    Add Country model

---
+++ src/Data/CountryJson.cs
@@ -0,0 +1,13 @@
+﻿namespace Nullforce.Api.DeviantArt.JsonModels
+{
+    public class CountriesRootJson
+    {
+        public CountryJson[] Results { get; set; }
+    }
+
+    public class CountryJson
+    {
+        public long CountryId { get; set; }
+        public string Name { get; set; }
+    }
+}
