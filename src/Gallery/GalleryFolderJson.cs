7d92a019dfc7f7435d39a59e6cac1c13f44afef8
nullforce <glenngit@nullforce.com>
2019-05-18 13:41:22 -0500

    Update gallery json

--- src/Gallery/GalleryFolderJson.cs
+++ src/Gallery/GalleryFolderJson.cs
@@ -8,5 +8,7 @@ namespace Nullforce.Api.DeviantArt.JsonModels
         [JsonProperty("Parent")]
         public string ParentFolderId { get; set; }
         public string Name { get; set; }
+        public long? Size { get; set; }
+        public DeviationJson[] Deviations { get; set; }
     }
 }
