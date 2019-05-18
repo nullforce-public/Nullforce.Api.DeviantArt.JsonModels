81cc230e823a9681cdc2b5f632372ecb7ec935aa
nullforce <glenngit@nullforce.com>
2019-05-18 10:35:09 -0500

    Add auth and placebo models

---
+++ src/Utility/PlaceboJson.cs
@@ -0,0 +1,7 @@
+namespace Nullforce.Api.DeviantArt.JsonModels
+{
+    public class PlaceboJson
+    {
+        public string Status { get; set; }
+    }
+}
---
+++ tests/Nullforce.Api.DeviantArt.JsonModels.Tests/IntegrationTests.cs
@@ -0,0 +1,31 @@
+using System;
+using Flurl;
+using Flurl.Http;
+using Xunit;
+
+namespace Nullforce.Api.DeviantArt.JsonModels.Tests
+{
+    [Trait("Category", "IntegrationTests")]
+    public class IntegrationTests
+    {
+        [Fact]
+        public async void Test1()
+        {
+            var clientId = Environment.GetEnvironmentVariable("DEVIANTART_CLIENTID");
+            var clientSecret = Environment.GetEnvironmentVariable("DEVIANTART_CLIENTSECRET");
+
+            var authUri = "https://www.deviantart.com/oauth2/token";
+            authUri = authUri.SetQueryParams(new {
+                grant_type = "client_credentials",
+                client_id = clientId,
+                client_secret = clientSecret
+            });
+
+            var authResult = await authUri.GetJsonAsync<AuthJson>();
+
+            var uri = "https://www.deviantart.com/api/v1/oauth2/placebo";
+
+            var result = await uri.WithOAuthBearerToken(authResult.AccessToken).GetJsonAsync<PlaceboJson>();
+        }
+    }
+}
